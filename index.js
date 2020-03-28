d2e87af9eccbb9d910fb124afaf7474fda79e8e2
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-03-28 22:15:27 +0300

    feat: add simple website

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((request, response) => {
-    response.end("Hello NodeJS!");
+
+    let filePath = path.join(__dirname, "src", request.url === "/" ? "index.html" : request.url);
+    const extension = path.extname(filePath);
+
+    if (!extension) {
+        filePath += ".html";
+    }
+
+    console.log(filePath);
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            fs.readFile(path.join(__dirname, "public", "error.html"), (err, data) => {
+                if (err) {
+                    response.writeHead(500);
+                    response.end("Server error.");
+                } else {
+                    response.writeHead(200, {
+                        "Content-Type": "text/html"
+                    });
+                    response.end(data);
+                }
+            })
+        } else {
+            response.writeHead(200, {
+                "Content-Type": "text/html"
+            });
+            response.end(content);
+        }
+    });
+
 });
 
 server.listen("5000", () => {
